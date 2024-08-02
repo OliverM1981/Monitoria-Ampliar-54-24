2f45ecbab5a739c77c2b7288db46470a66d2a1fb
Mauricio De Oliveira <manutencaomauricio81@gmail.com>
2024-08-02 20:54:16 -0300

    cleanup: remove outdated or redundant Python code files including various data structures and algorithms from Python Codes directory

--- Python Codes/data_structures/is_rotation.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# Author: GuoChen
-# Title:  Unique Character search
-# Problem: Assume you have a method isSubstring which checks if one word is a
-#           substring of another.
-##############################################################################
--- Python Codes/data_structures/list.py
+++
@@ -1,9 +0,0 @@
-def main():
-    fruits = ["Apple", "Banana", "Cherry"]
-
-    for fruit in fruits:
-        print(fruit)
-        
-
-if __name__ == "__main__":
-    main()
